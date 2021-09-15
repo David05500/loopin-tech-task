7e4054e26d5eaa2115b70409f3ca5d03cc6594ce
David Sarvasidze <sharvashidze.david@gmail.com>
2021-09-15 11:07:35 +0100

    feat/removed hardcoded children from Card.stories

--- components/Card/__stories__/Card.stories.tsx
+++ components/Card/__stories__/Card.stories.tsx
@@ -9,27 +9,7 @@ export default {
 
 const Default: ComponentStory<typeof Card> = (args) => (
 	<Card {...args}>
-		<div className="flex flex-col">
-			<div className="text-xl mb-4 font-bold">
-				Card sub-title
-			</div>
-
-			<div className="flex justify-between">
-				<div className="w-1/2 mr-2 border rounded-md border-borderColor p-2 md:p-4">
-					<h3 className="text-sm md:text-base mb-4 font-semibold">Section 1</h3>
-					<p className="text-3xl font-semibold">Section 1 content</p>
-				</div>
-
-				<div className="w-1/2 ml-2 border rounded-md border-borderColor p-2 md:p-4">
-					<h3 className="text-sm md:text-base mb-4 font-semibold">Section 2</h3>
-					<p className="text-3xl font-semibold">Section 1 content</p>
-				</div>
-			</div>
-
-			<button className="text-sm text-center mt-4 text-green font-semibold">
-				action button
-			</button>
-		</div>
+		{args.children}
 	</Card>
 );
 
@@ -41,24 +21,28 @@ Primary.args = {
 	img1src: 'https://via.placeholder.com/374x131.png?text=image',
 	img1Alt: "Image 1 alt",
 	img2src: 'https://via.placeholder.com/374x131.png?text=image',
-	img2Alt: "Image 2 alt"
+	img2Alt: "Image 2 alt",
+	children: <div>Content</div>
 }
 
 export const Negative = Default.bind({})
 Negative.args = {
 	title: "Negative Card",
-	variant: "negative"
+	variant: "negative",
+	children: <div>Content</div>
 }
 
 export const Neutral = Default.bind({})
 Neutral.args = {
 	title: "Neutral Card",
-	variant: "neutral"
+	variant: "neutral",
+	children: <div>Content</div>
 }
 
 export const Positive = Default.bind({})
 Positive.args = {
 	title: "Positive Card",
 	description: "Positive Card Description",
-	variant: "positive"
+	variant: "positive",
+	children: <div>Content</div>
 }
